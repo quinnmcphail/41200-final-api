530abca5c5d387be3ba32039071f399baceb1433
Quinn McPhail <quinnmcphail@me.com>
2019-05-01 15:03:06 -0400

    Finished Product CRUD

--- app/routes/Product.route.js
+++ app/routes/Product.route.js
@@ -1,12 +1,12 @@
 module.exports = app => {
   const products = require("../controllers/Product.controller.js");
-  // app.post("/products", products.create);
+  app.post("/products", products.create);
   app.get("/products", products.findAll);
   app.get("/products/categories/", products.findAllCategories);
   app.get("/products/categories/:categoryName", products.findProductsByCategory);
   app.get("/products/subcategories/", products.findAllSubcategories);
   app.get("/products/subcategories/:subCategoryName", products.findProductsBySubcategory);
   app.get("/products/:productId", products.findOne);
-  // app.put("/products/:productId", products.update);
-  // app.delete("/products/:productId", products.delete);
+  app.put("/products/:productId", products.update);
+  app.delete("/products/:productId", products.delete);
 };
