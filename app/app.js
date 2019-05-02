d28becf172eeb7a70fe502c841e99807d72ddde5
Quinn McPhail <quinnmcphail@me.com>
2019-05-01 22:59:26 -0400

    Added CORS

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dbConfig = require("./config/database.config.js");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.Promise = global.Promise;
 mongoose.set("useFindAndModify", false);
@@ -23,6 +24,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "GroceryList API" });
